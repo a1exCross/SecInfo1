be9b7eb0d84f3df1728841e8f9e0598d4c93c857
a1exCross <70817560+a1exCross@users.noreply.github.com>
2022-02-16 01:53:38 +0300

    1

--- SecInfo1/Form1.Designer.cs
+++ SecInfo1/Form1.Designer.cs
@@ -28,58 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(40, 220);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(382, 91);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(181, 333);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(40, 44);
+            this.richTextBox2.Location = new System.Drawing.Point(43, 204);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(382, 91);
+            this.richTextBox2.Size = new System.Drawing.Size(532, 91);
             this.richTextBox2.TabIndex = 2;
             this.richTextBox2.Text = "";
             this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(257, 325);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ОК";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Проверка подключения к интернету";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Проверка лицензии";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(470, 383);
+            this.ClientSize = new System.Drawing.Size(470, 387);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.richTextBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
             this.Text = "Получение лицензии";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.VisibleChanged += new System.EventHandler(this.Form1_VisibleChanged);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- SecInfo1/Form1.cs
+++ SecInfo1/Form1.cs
@@ -22,8 +22,12 @@ namespace SecInfo1
         public Form1()
         {
             InitializeComponent();
-            Init();
+            button1.Visible = false;
+            richTextBox2.Visible = false;
+            this.Size = new System.Drawing.Size(488, 200);
+            label2.Visible = false;
         }
+
         public string Hash(string password)
         {
             byte[] data = Encoding.Default.GetBytes(password);
@@ -41,53 +45,21 @@ namespace SecInfo1
 
         }
 
-        public void insert(string p1, string p2, int key)
+        public string read(string p1)
         {
             try
             {
                 NpgsqlConnection conn = new NpgsqlConnection("Server = 127.0.0.1; Port = 5432; User Id = postgres; Password = postgres; Database = secinf");
                 conn.Open();
-                NpgsqlCommand com;
-                if (key == 0)
-                    com = new NpgsqlCommand("INSERT INTO public.Processor(proc_id,proc_name) VALUES(@p1, @p2)", conn);
-                else com = new NpgsqlCommand("INSERT INTO public.harddrive(hard_model,hard_serial) VALUES(@p1, @p2)", conn);
-                var a = new NpgsqlParameter("@p1", NpgsqlTypes.NpgsqlDbType.Varchar);
-                var b = new NpgsqlParameter("@p2", NpgsqlTypes.NpgsqlDbType.Varchar);
 
-                a.Value = Hash(p1);
-                b.Value = Hash(p2);
-
-                com.Parameters.Add(a);
-                com.Parameters.Add(b);
-
-                com.ExecuteNonQuery();
-
-
-                conn.Close();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-
-        public string read(string p1, int key)
-        {
-            try
-            {
-                NpgsqlConnection conn = new NpgsqlConnection("Server = 127.0.0.1; Port = 5432; User Id = postgres; Password = postgres; Database = secinf");
-                conn.Open();
-                NpgsqlCommand com;
-
-                if (key == 0)
-                     com = new NpgsqlCommand("SELECT proc_id from processor where processor.proc_name='"+ Hash(p1)+"'", conn);
-                else com = new NpgsqlCommand("SELECT hard_serial from harddrive where harddrive.hard_model='" + Hash(p1) + "'", conn);
+                NpgsqlCommand com = new NpgsqlCommand("SELECT user_token from license where license.proc_id='" + p1 + "'", conn); ;
 
                 NpgsqlDataReader reader;
                 reader = com.ExecuteReader();
+
                 string[,] nums = new string[100, 100];
                 int a = 0;
-
+                
                 while (reader.Read())
                 {
                     try
@@ -114,7 +86,59 @@ namespace SecInfo1
 
         }
 
-        string veryf = "";
+        public bool regedit_check(string p1, string p2, string veryf, bool license)
+        {
+            RegistryKey currentUserKey = Registry.CurrentUser;
+            if (currentUserKey.OpenSubKey("Application", true) != null)
+            {
+                RegistryKey Key = currentUserKey.OpenSubKey("Application", true);
+                if (Key.GetValue(Hash(p1 + p2)) != null)
+                {
+                    if (veryf != Key.GetValue(Hash(p1 + p2)).ToString())
+                        license = false;
+                }
+                else
+                    license = false;
+
+                Key.Close();
+            }
+            else return false;
+
+            return license; 
+        }
+
+        public bool regedit(string p1, string p2, string veryf, bool license)
+        {
+            RegistryKey currentUserKey = Registry.CurrentUser;
+            if (currentUserKey.OpenSubKey("Application", true) != null)
+            {
+                RegistryKey Key = currentUserKey.OpenSubKey("Application", true);
+                if (Key.GetValue(Hash(p1 + p2)) != null)
+                {
+                    if (veryf != Key.GetValue(Hash(p1 + p2)).ToString())
+                        license = false;
+                }
+                else
+                if (veryf == Hashh(Hash(p1 + p2)))
+                {
+                    Key.SetValue(Hash(p1 + p2), veryf);
+                    set = true;
+                }
+
+                Key.Close();
+            }
+            else
+            {
+                RegistryKey Key = currentUserKey.CreateSubKey("Application");
+                Key.SetValue(Hash(p1 + p2), veryf);
+                Key.Close();
+            }
+
+            return license;
+
+        }
+        bool active = false;
+        bool set = false;
 
         private void Init()
         {
@@ -122,30 +146,16 @@ namespace SecInfo1
             string name = null;
             string h_id = null;
             string serial = null;
-
-            //IPStatus status = IPStatus.Unknown;
-            //try
-            //{
-            //    Ping p = new Ping();
-            //    PingReply pr = p.Send(@"google.com");
-            //    status = pr.Status;
-            //}
-            //catch { }
-            //if (status == IPStatus.Success)
-            //{
-            //    MessageBox.Show("Подключение к интернету установлено");
-            //}
-            //else
-            //{
-            //    MessageBox.Show("Проверьте подключение к интернету!");
-            //}
+            string veryf = null;
+            active = true;
+            bool license = true;
+            bool check = false;
 
             ManagementObjectSearcher searcher =
                    new ManagementObjectSearcher("root\\CIMV2",
                    "SELECT * FROM Win32_DiskDrive");
             foreach (ManagementObject queryObj in searcher.Get())
             {
-                h_id = queryObj["Model"].ToString();
                 serial = queryObj["SerialNumber"].ToString();
             }
 
@@ -155,77 +165,85 @@ namespace SecInfo1
             foreach (ManagementObject queryObj in searcher1.Get())
             {
                 pid = queryObj["ProcessorId"].ToString();
-                name = queryObj["Name"].ToString();
             }
 
-            veryf = serial + pid;
-            richTextBox2.Text = Hash(veryf);
-            //richTextBox1.Text = "asd";
+            veryf = read(Hash(serial + pid));
 
-            //MessageBox.Show("processor: " + pid + '\n' + name + "\nHardDrive: " + h_id + '\n' + serial, "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-            //insert(pid, name, 0);
-            //insert(h_id, serial, 1);
-       
-            //string veryf_proc = read(name, 0);
-            //string veryf_hard = read(h_id, 1);
-
-            //RegistryKey currentUserKey = Registry.CurrentUser;
-
-            bool license = true;
+            label1.Text = "Проверка подключения к интернету";
+            Application.DoEvents();
+            Thread.Sleep(500);
 
 
+            label2.Visible = true;
+            label2.Text = "Проверка лицензии";
+            Application.DoEvents();
+            Thread.Sleep(500);
 
-            //if (veryf_proc != null && veryf_hard != null)
-            //{
-            //    if (currentUserKey.OpenSubKey("Application", true) != null)
-            //    {
-            //        RegistryKey Key = currentUserKey.OpenSubKey("Application",true);
-            //        if (Key.GetValue(Hash("KEY")) != null)
-            //        {
-            //            if (veryf_proc != Key.GetValue(Hash("KEY")).ToString())
-            //                license = false;
-            //        }
-            //        else
-            //        if (veryf_proc == Hash(pid) && veryf_hard == Key.GetValue(Hash("KEYY")).ToString())
-            //        {
-            //            Key.SetValue(Hash("KEY"), veryf_proc);
-            //            license = true;
-            //        }
-            //        else license = false;
+            if (!regedit_check(serial, pid, veryf, license))
+            {
 
-            //        if (Key.GetValue(Hash("KEYY")) != null)
-            //        {
-            //            if (veryf_hard != Key.GetValue(Hash("KEYY")).ToString())
-            //                license = false;
-            //        }
-            //        else
-            //        if (veryf_hard == Hash(serial) && veryf_proc == Key.GetValue(Hash("KEY")).ToString())
-            //        {
-            //            Key.SetValue(Hash("KEYY"), veryf_hard);
-            //            license = true;
-            //        }
-            //        else license = false;
+                IPStatus status = IPStatus.Unknown;
+                try
+                {
+                    Ping p = new Ping();
+                    PingReply pr = p.Send(@"google.com");
+                    status = pr.Status;
+                }
+                catch { }
+                if (status == IPStatus.Success)
+                {
+                    label1.Text = "Подключение к интернету установлено";
+                }
+                else
+                {
+                    label1.Text = "Проверьте подключение к интернету!";
+                    return;
+                }
 
-            //        Key.Close();
-            //    }
-            //    else
-            //    {
-            //        if (veryf_proc == Hash(pid) && veryf_hard == Hash(serial))
-            //        {
-            //            RegistryKey Key = currentUserKey.CreateSubKey("Application");
-            //            Key.SetValue(Hash("KEY"), veryf_proc);
-            //            Key.SetValue(Hash("KEYY"), veryf_hard);
-            //            Key.Close();
-            //        }
-            //        else license = false;
-            //    }
-            //}
-            //if (!license)
-            //{
-            //    MessageBox.Show("Данная копия программы не является лицензионной! Обратитесь к разработчку для приобритения лицензии!","Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            //    Process.GetCurrentProcess().Kill();
-            //}    
+                richTextBox2.Text = Hash(serial + pid);
+
+                //MessageBox.Show("processor: " + pid + '\n' + name + "\nHardDrive: " + h_id + '\n' + serial, "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                if (veryf == null)
+                {
+                    label2.Text = "Запуск программы невозможен.\nДля получения лицензии отправьте указанный ключ ниже разработчикам программы\nПрограмма активируется автоматически";
+                    button1.Visible = true;
+                    richTextBox2.Visible = true;
+                    this.Size = new System.Drawing.Size(488, 330);
+                    return;
+                }
+                else
+                {
+                    if (veryf == Hashh(Hash(serial + pid)))
+                        license = regedit(serial, pid, veryf, license);
+                    else license = false;
+                }
+
+                if (license)
+                {
+                    if (!set)
+                        label2.Text = "Программа является лицензионной";
+                    else label2.Text = "Лицензия установлена";
+
+                    Thread.Sleep(500);
+                    Application.DoEvents();
+                }
+                else
+                {
+                    MessageBox.Show("Данная копия программы не является лицензионной! Обратитесь к разработчку для приобритения лицензии!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Process.GetCurrentProcess().Kill();
+                }
+            }
+            else
+            {
+                //MessageBox.Show("hmm");
+                label2.Text = "Программа является лицензионной";
+                Thread.Sleep(500);
+                this.Hide();
+                Form2 f2 = new Form2();
+                f2.ShowDialog();
+
+            }
 
         }
 
@@ -234,16 +252,34 @@ namespace SecInfo1
 
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.GetCurrentProcess().Kill();
+        }
+
         private void richTextBox2_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            if (richTextBox1.Text == Hashh(Hash(veryf)))
-                MessageBox.Show("OK", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+           
+        }
 
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+           if (!active) Init();
+        }
+
+        private void Form1_VisibleChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
